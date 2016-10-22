a2c5296a205a2dde51e46dbd249680c05809118e
Ignacio Gobet <ignaciogobet@gmail.com>
2016-10-22 15:01:48 -0300

    varios arreglos con luquitas

--- src/main/java/com/unounocuatro/ducit/utils/DucitUtils.java
+++ src/main/java/com/unounocuatro/ducit/utils/DucitUtils.java
@@ -15,9 +15,9 @@ public class DucitUtils {
 		WritableRaster raster = bi.copyData(null);
 		return new BufferedImage(cm, raster, isAlphaPremultiplied, null);
 	}
-	
+
 	public static BufferedImage mat2Img(Mat in)
-    {
+	{
 		//Imgproc.cvtColor(in, in, Imgproc.COLOR_RGB2GRAY, 0);
 
 		// Create an empty image in matching format
@@ -26,10 +26,10 @@ public class DucitUtils {
 		// Get the BufferedImage's backing array and copy the pixels directly into it
 		byte[] data = ((DataBufferByte) gray.getRaster().getDataBuffer()).getData();
 		in.get(0, 0, data);
-		
+
 		return gray;
-    }
-	
+	}
+
 	public static BufferedImage cleanTrash(BufferedImage image){
 		for(int i=0; i<image.getWidth(); i++){
 			int whites = 0;
@@ -50,7 +50,7 @@ public class DucitUtils {
 				}
 			}
 		}
-		
+
 		for(int i=0; i<image.getWidth(); i++){
 			int whites = 0;
 			boolean allowed = true;
@@ -70,7 +70,7 @@ public class DucitUtils {
 				}
 			}
 		}
-		
+
 		return image;
 	}
 
@@ -114,12 +114,12 @@ public class DucitUtils {
 	public static String[] getStringArray(String result) {
 		String[] words = result.split("\\s+");
 		for (int i = 0; i < words.length; i++) words[i] = words[i].replaceAll("[^\\w]", "");
-		
+
 		return words;
 	}
 
 	public static String cleanText(String text) {	
-		
+
 
 		if(text.length()>2){
 			if(Character.isDigit(text.charAt(0)))
@@ -128,7 +128,7 @@ public class DucitUtils {
 				text = text.substring(0, text.length()-1);
 			for(int i=1; i<text.length()-1; i++){
 				if(Character.isLetter(text.charAt(i-1)) && Character.isLetter(text.charAt(i+1)) && text.charAt(i) == '¡')
-						text = text.substring(0, i) + 'i' + text.substring(i+1,text.length());
+					text = text.substring(0, i) + 'i' + text.substring(i+1,text.length());
 				/*if(Character.isDigit(text.charAt(i-1)))
 						text = text.substring(0, i-1) + text.substring(i,text.length());
 				if(Character.isDigit(text.charAt(i+1)))
@@ -139,11 +139,11 @@ public class DucitUtils {
 		text = text.replaceAll("^\\s+", "");
 		text = text.replaceAll("\\s+$", "");
 		text = text.replaceAll("á", "a").replaceAll("é", "e").replaceAll("í", "i").replaceAll("ó", "o").replaceAll("ú", "u");
-		
-		
-		
+
+
+
 		return text;
-		
+
 	}
 
 	public static String getSQLPattern(String word) {
@@ -158,13 +158,13 @@ public class DucitUtils {
 		}
 		return result;
 	}
-	
-	public static int getLevenshteinDistance(String s, String t) {
-	      if (s == null || t == null) {
-	          throw new IllegalArgumentException("Strings must not be null");
-	      }
 
-	      /*
+	public static int getLevenshteinDistance(String s, String t) {
+		if (s == null || t == null) {
+			throw new IllegalArgumentException("Strings must not be null");
+		}
+
+		/*
 	         The difference between this impl. and the previous is that, rather 
 	         than creating and retaining a matrix of size s.length()+1 by t.length()+1, 
 	         we maintain two single-dimensional arrays of length s.length()+1.  The first, d,
@@ -179,88 +179,90 @@ public class DucitUtils {
 
 	         Effectively, the difference between the two implementations is this one does not 
 	         cause an out of memory condition when calculating the LD over two very large strings.
-	       */
+		 */
 
-	      int n = s.length(); // length of s
-	      int m = t.length(); // length of t
+		int n = s.length(); // length of s
+		int m = t.length(); // length of t
 
-	      if (n == 0) {
-	          return m;
-	      } else if (m == 0) {
-	          return n;
-	      }
+		if (n == 0) {
+			return m;
+		} else if (m == 0) {
+			return n;
+		}
 
-	      if (n > m) {
-	          // swap the input strings to consume less memory
-	          String tmp = s;
-	          s = t;
-	          t = tmp;
-	          n = m;
-	          m = t.length();
-	      }
+		if (n > m) {
+			// swap the input strings to consume less memory
+			String tmp = s;
+			s = t;
+			t = tmp;
+			n = m;
+			m = t.length();
+		}
 
-	      int p[] = new int[n+1]; //'previous' cost array, horizontally
-	      int d[] = new int[n+1]; // cost array, horizontally
-	      int _d[]; //placeholder to assist in swapping p and d
+		int p[] = new int[n+1]; //'previous' cost array, horizontally
+		int d[] = new int[n+1]; // cost array, horizontally
+		int _d[]; //placeholder to assist in swapping p and d
 
-	      // indexes into strings s and t
-	      int i; // iterates through s
-	      int j; // iterates through t
+		// indexes into strings s and t
+		int i; // iterates through s
+		int j; // iterates through t
 
-	      char t_j; // jth character of t
+		char t_j; // jth character of t
 
-	      int cost; // cost
+		int cost; // cost
 
-	      for (i = 0; i<=n; i++) {
-	          p[i] = i;
-	      }
+		for (i = 0; i<=n; i++) {
+			p[i] = i;
+		}
 
-	      for (j = 1; j<=m; j++) {
-	          t_j = t.charAt(j-1);
-	          d[0] = j;
+		for (j = 1; j<=m; j++) {
+			t_j = t.charAt(j-1);
+			d[0] = j;
 
-	          for (i=1; i<=n; i++) {
-	              cost = s.charAt(i-1)==t_j ? 0 : 1;
-	              // minimum of cell to the left+1, to the top+1, diagonally left and up +cost
-	              d[i] = Math.min(Math.min(d[i-1]+1, p[i]+1),  p[i-1]+cost);
-	          }
+			for (i=1; i<=n; i++) {
+				cost = s.charAt(i-1)==t_j ? 0 : 1;
+				// minimum of cell to the left+1, to the top+1, diagonally left and up +cost
+				d[i] = Math.min(Math.min(d[i-1]+1, p[i]+1),  p[i-1]+cost);
+			}
 
-	          // copy current distance counts to 'previous row' distance counts
-	          _d = p;
-	          p = d;
-	          d = _d;
-	      }
+			// copy current distance counts to 'previous row' distance counts
+			_d = p;
+			p = d;
+			d = _d;
+		}
 
-	      // our last action in the above loop was to switch d and p, so p now 
-	      // actually has the most recent cost counts
-	      return p[n];
-	  }
+		// our last action in the above loop was to switch d and p, so p now 
+		// actually has the most recent cost counts
+		return p[n];
+	}
 
 	public static String cleanPlainText(String result) {
 		if(result == null)
 			return "";
 		String textArray[] = result.split("\\r\\n|\\n|\\r");
-		
+
 		for(int i=0; i<textArray.length; i++){
 			int errors = 0;
 			int size = textArray[i].length();
 			for(int j=1; j<size -1; j++){
+				if(textArray[i].charAt(j) == '|' && Character.isLetter(textArray[i].charAt(j-1)) && Character.isLetter(textArray[i].charAt(j+1)))
+					textArray[i] = textArray[i].substring(0, j) + 'l' + textArray[i].substring(j+1,textArray[i].length());
 				if(isBadChar(textArray[i].charAt(j), textArray[i].charAt(j-1), textArray[i].charAt(j+1)))
-					errors++;							
+					errors++;
 			}
 
 			if((float)errors/size > 0.1)
 				textArray[i] = "El segmento en esta posición fue detectado defectuosamente.";	
 		}
-		
+
 		return getTextFromString(textArray);
 	}
 
 	private static String getTextFromString(String[] textArray) {
 		StringBuilder strBuilder = new StringBuilder();
 		for (int i = 0; i < textArray.length; i++) {
-		   strBuilder.append(textArray[i]);
-		   strBuilder.append(System.getProperty("line.separator"));
+			strBuilder.append(textArray[i]);
+			strBuilder.append(System.getProperty("line.separator"));
 		}
 		return strBuilder.toString();
 	}
@@ -271,61 +273,72 @@ public class DucitUtils {
 				return true;
 			}
 		}
-			
+
 		switch(actual){
-			case '°':
+		case '°':
+			return true;
+		case '"':
+		{
+			// Ejemplos que arrojarían error:  PEPE "!STA	 |  TAMBI"N
+			if((previous == ' ' && !Character.isLetter(next)) || Character.isLetter(previous) && !Character.isLetter(next))
 				return true;
-			case '"':
-			{
-				// Ejemplos que arrojarían error:  PEPE "!STA	 |  TAMBI"N
-				if((previous == ' ' && !Character.isLetter(next)) || Character.isLetter(previous) && !Character.isLetter(next))
-					return true;
-				if(next == '.')
-					return true;
-			}
-			case '¡':
-			{
-				if(!Character.isLetter(next))
-					return true;
-			}
-			case '!':
-			{
-				if(!Character.isLetter(previous))
-					return true;
-			}
-			case '-':
-			{
-				if(previous == '.' || next == '.')
-					return true;
-				if(Character.isLetterOrDigit(previous) && next =='\\' )
-					return true;
-				if(Character.isLetter(previous) && next == ' ')
-					return true;
-			}
-			case '\'':
-				// Ejemplos que arrojarían error:  PEPE '!STA	 |  TAMBI'N
-				if((previous == ' ' && !Character.isLetter(next)) || (Character.isLetter(previous) && !Character.isLetter(next)))
-					return true;
-			case '.':
-				if (previous == '.' && Character.isLetter(next))
-					return true;
-				if (previous == ' ' && next == '.')
-					return true;
-				if (previous == '¡' || next == '¡')
-					return true;
-				if (previous == '-' ||next == '-')
-					return true;
-			case '#':
+			if(next == '.')
 				return true;
-			case '@':
+		}
+		case '¡':
+		{
+			if(!Character.isLetter(next))
 				return true;
-			case '|':
+		}
+		case '!':
+		{
+			if(!Character.isLetter(previous))
 				return true;
-			case '¿':
-				if (Character.isLetter(previous) || !Character.isLetter(next))
-					return true;
-			}
-			return false;
+		}
+		case '-':
+		{
+			if(previous == '.' || next == '.')
+				return true;
+			if(Character.isLetterOrDigit(previous) && next =='\\' )
+				return true;
+			if(Character.isLetter(previous) && next == ' ')
+				return true;
+		}
+		case '\'':
+			// Ejemplos que arrojarían error:  PEPE '!STA	 |  TAMBI'N
+			if((previous == ' ' && !Character.isLetter(next)) || (Character.isLetter(previous) && !Character.isLetter(next)))
+				return true;
+		case '.':
+			if (previous == '.' && Character.isLetter(next))
+				return true;
+			if (previous == ' ' && next == '.')
+				return true;
+			if (previous == '¡' || next == '¡')
+				return true;
+			if (previous == '-' ||next == '-')
+				return true;
+		case '#':
+			return true;
+		case '@':
+			return true;
+		case '|':
+			return true;
+		case '¿':
+			if (Character.isLetter(previous) || !Character.isLetter(next))
+				return true;
+
+		case ';':
+			if(next == ';' || previous == ';')
+				return true;
+			else if(Character.isLetter(previous) && Character.isLetter(next))
+				return true;
+		case '&':
+			if(Character.isLetter(previous) && Character.isLetter(next))
+				return true;
+		case '—':
+			return true;
+		}
+		return false;
 	}
 
 	public static String cleanWikiText(String text) {
@@ -337,7 +350,7 @@ public class DucitUtils {
 				text = text.substring(0, i) + text.substring(i+26, text.length()-1);
 			i = text.indexOf("<span ");
 		}		
-		
+
 		return text;
 	}	
 }
